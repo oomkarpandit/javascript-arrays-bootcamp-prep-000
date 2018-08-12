c1f296b3f1701e807a018f9a7220de75411fb312
Oomkar Pandit <oomkar@yahoo.com>
2018-08-12 14:53:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,4 +24,17 @@ function accessElementInArray(array,index){
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
   array.shift()
+  return array
+}
+function removeElementFromBeginningOfArray(array){
+  return array.slice(1)
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+function removeElementFromEndOfArray(array){
+var array1=  array.slice(0,array.length-1)
+  return array1
+  
 }
